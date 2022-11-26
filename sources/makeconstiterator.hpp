576a2667ab5f179500cc1f3d09d2c55667ed6bb7
Nico Josuttis <nico@josuttis.de>
2022-11-26 14:43:49 +0100

    clean-up own implementation of basic_const_iterator<> and make_const_iterator()

--- sources/makeconstiterator.hpp
+++ sources/makeconstiterator.hpp
@@ -10,27 +10,15 @@
 
 namespace belleviews::_intern {
 
-template <typename It> struct iterator_concept_for { };
-template <typename It> requires std::contiguous_iterator<It>
-struct iterator_concept_for<It> {
-    using iterator_concept = std::contiguous_iterator_tag;
-};
 
-template <typename It> struct iterator_category_for { };
-template <std::forward_iterator It>
-struct iterator_category_for<It> {
-    using iterator_category = typename std::iterator_traits<It>::iterator_category;
-};
-
-template <typename T, typename U>
-concept NotSameAs = not std::same_as<T, U>;
-
-// Given some type I, the concept not-a-const-iterator is defined as
-// - false if I is a specialization of basic_const_iterator and
-// - true otherwise.
 template <std::input_iterator Iterator>
 class basic_const_iterator;
 
+
+// concept NotAConstIterator:
+//   Given some type I, the concept not-a-const-iterator is defined as
+//   - false if I is a specialization of basic_const_iterator and
+//   - true otherwise.
 template<typename _Tp>
 inline constexpr bool is_not_a_const_iterator = false;
 
@@ -41,18 +29,48 @@ template <typename Iterator>
 concept NotAConstIterator = is_not_a_const_iterator<Iterator>;
 
 
+// The member typedef-name iterator_category is defined if and only if Iterator models forward_iterator.
+// In that case, basic_const_iterator<Iterator>::iterator_category denotes the type
+// iterator_traits<Iterator>::iterator_category.
+template<typename It>
+struct const_iterator_iter_cat {
+};
+template <std::forward_iterator It>
+struct const_iterator_iter_cat<It> {
+    using iterator_category = typename std::iterator_traits<It>::iterator_category;
+};
+
+
 template <std::input_iterator Iterator>
-class basic_const_iterator
-// TODO:
-//class basic_const_iterator : public iterator_concept_for<Iterator>
-//                           , public iterator_category_for<Iterator>
+class basic_const_iterator : public const_iterator_iter_cat<Iterator>
 {
-  Iterator current_ = Iterator(); // exposition only
-  using reference = ::belleviews::_intern::iter_const_reference_t<Iterator>; // exposition only
+ private:
+  // basic_const_iterator<Iterator>::iterator_concept is defined as follows:
+  // - If Iterator models contiguous_iterator, then iterator_concept denotes contiguous_iterator_tag.
+  // - Otherwise, if Iterator models random_access_iterator, then iterator_concept denotes random_access_iterator_tag.
+  // - Otherwise, if Iterator models bidirectional_iterator, then iterator_concept denotes bidirectional_iterator_tag.
+  // - Otherwise, if Iterator models forward_iterator, then iterator_concept denotes forward_iterator_tag.
+  // - Otherwise, iterator_concept denotes input_iterator_tag.
+  static auto _S_iter_concept() {
+    if constexpr (std::contiguous_iterator<Iterator>)
+      return std::contiguous_iterator_tag{};
+    else if constexpr (std::random_access_iterator<Iterator>)
+      return std::random_access_iterator_tag{};
+    else if constexpr (std::bidirectional_iterator<Iterator>)
+      return std::bidirectional_iterator_tag{};
+    else if constexpr (std::forward_iterator<Iterator>)
+      return std::forward_iterator_tag{};
+    else
+      return std::input_iterator_tag{};
+  }
+
+private:
+  Iterator current_ = Iterator();                                             // exposition only
+  using reference = ::belleviews::_intern::iter_const_reference_t<Iterator>;  // exposition only
 
 public:
-  //using iterator_concept = see below ;
-  //using iterator_category = see below ; // not always present
+  using iterator_concept = decltype(_S_iter_concept());
+  //using iterator_category = see below ; // not always present (defiined via base type)
   using value_type = std::iter_value_t<Iterator>;
   using difference_type = std::iter_difference_t<Iterator>;
 
@@ -130,66 +148,31 @@ public:
   }
   
   // - :
-#ifdef OLDOPMINUS
   friend constexpr basic_const_iterator operator-(const basic_const_iterator& i, difference_type n)
   requires std::random_access_iterator<Iterator> {
     return basic_const_iterator(i.current_ - n);
   }
 
-    template <std::sized_sentinel_for<Iterator> S>
-    auto operator-(S const& s) const -> std::iter_difference_t<Iterator> {
-        return current_ - s;
-    }
-
-    template <NotSameAs<basic_const_iterator> S>
-        requires std::sized_sentinel_for<S, Iterator>
-    friend auto operator-(S const& s, basic_const_iterator const& rhs) -> std::iter_difference_t<Iterator> {
-        return s - rhs.current_;
-    }
-    //auto operator-(difference_type n) const -> basic_const_iterator requires std::random_access_iterator<Iterator> { return const_iterator(current_ - n); }
-  
-  // HERE IS A PROBLEM:
-  //template<std::sized_sentinel_for<Iterator> S>
-  //friend constexpr difference_type operator-(const basic_const_iterator& i, const S& y) {
-  //  return basic_const_iterator(i.current_ - y);
-  //}
-  
-  //template<std::sized_sentinel_for<Iterator> S>
-  //requires belleviews::_intern::different_from<S, basic_const_iterator>
-  //friend constexpr difference_type operator-(const S& x, const basic_const_iterator& y) {
-  //  return x - y.current_;
-  //}
-    //auto operator-(basic_const_iterator const& rhs) const -> difference_type requires std::random_access_iterator<Iterator> { return current_ - rhs.current_; }
-#else
-  friend constexpr basic_const_iterator operator-(const basic_const_iterator& i, difference_type n)
-  requires std::random_access_iterator<Iterator> {
-    return basic_const_iterator(i.current_ - n);
-  }
-
+  // STD:
+  // TODO: as friend function this yields an ERROR on gcc: 
   //template<std::sized_sentinel_for<Iterator> S>
   //friend constexpr difference_type operator-(const basic_const_iterator& x, const S& y) {
   //  return x.current_ - y;
   //}
-
-  //template<std::sized_sentinel_for<Iterator> S>
-  //requires belleviews::_intern::different_from<S, basic_const_iterator>
-  //friend constexpr difference_type operator-(const S& x, const basic_const_iterator& y) {
-  //  return x - y.current_;
-  //}
     template <std::sized_sentinel_for<Iterator> S>
-    auto operator-(S const& s) const -> std::iter_difference_t<Iterator> {
-        return current_ - s;
+    std::iter_difference_t<Iterator> operator-(S const& s) const {
+      return current_ - s;
     }
 
-    template <NotSameAs<basic_const_iterator> S>
-        requires std::sized_sentinel_for<S, Iterator>
-  //template<std::sized_sentinel_for<Iterator> S>
-  //requires belleviews::_intern::different_from<S, basic_const_iterator>
-  //requires NotSameAs<S, basic_const_iterator>
-    friend auto operator-(S const& s, basic_const_iterator const& rhs) -> std::iter_difference_t<Iterator> {
-        return s - rhs.current_;
+  // OOPS: it seems the order of the constraints matters:
+  //  template<std::sized_sentinel_for<Iterator> S> requires belleviews::_intern::different_from<S, basic_const_iterator>  // ERROR
+  //  template <typename S> requires belleviews::_intern::different_from<S, basic_const_iterator> && std::sized_sentinel_for<S, Iterator>  // OK
+  template <typename S>
+    //requires std::sized_sentinel_for<S, Iterator> && belleviews::_intern::different_from<S, basic_const_iterator>  // ERROR with gcc
+    requires belleviews::_intern::different_from<S, basic_const_iterator> && std::sized_sentinel_for<S, Iterator>    // OK
+    friend constexpr difference_type operator-(const S& x, basic_const_iterator const& y) {
+        return x - y.current_;
     }
-#endif
 
   // += and -= :
   constexpr basic_const_iterator& operator+=(difference_type n)
@@ -224,26 +207,6 @@ public:
     }
 #endif
 
-#ifdef OTHER
-    constexpr auto operator<=>(basic_const_iterator const& rhs) const
-      requires std::random_access_iterator<Iterator> {
-        return current_ <=> rhs;
-    }
-    template <NotSameAs<basic_const_iterator> Rhs>
-        requires std::random_access_iterator<Iterator> and std::totally_ordered_with<Iterator, Rhs>
-    auto operator<=>(Rhs const& rhs) const {
-        if constexpr (std::three_way_comparable_with<Iterator, Rhs>) {
-            return current_ <=> rhs;
-        } else if constexpr (std::sized_sentinel_for<Rhs, Iterator>) {
-            return (current_ - rhs) <=> 0;
-        } else {
-            if (current_ < rhs) return std::strong_ordering::less;
-            if (rhs < current_) return std::strong_ordering::greater;
-            return std::strong_ordering::equal;
-        }
-    }
-
-#else
   //STD
   friend constexpr bool operator<(const basic_const_iterator& x, const basic_const_iterator& y)
     requires std::random_access_iterator<Iterator> {
@@ -293,8 +256,7 @@ public:
       return x.current_ <=> y;
   }
 
-  //template<belleviews::_intern::NotAConstIterator I>
-  template<typename I>
+  template<belleviews::_intern::NotAConstIterator I>
   friend constexpr bool operator<(const I& x, const basic_const_iterator& y)
   requires std::random_access_iterator<Iterator> && std::totally_ordered_with<Iterator, I> {
       return x < y.current_;
@@ -315,7 +277,6 @@ public:
       return x >= y.current_;
   }
   // note: no operator<=> here
-#endif
   
 };
 
