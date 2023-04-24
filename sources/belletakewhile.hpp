09685b797a444d82c10146b921d351097ba58aa8
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2023-04-24 23:41:14 +0200

    Update take_while view description comment

--- sources/belletakewhile.hpp
+++ sources/belletakewhile.hpp
@@ -37,13 +37,8 @@
 //
 // A C++ view
 // with the following benefits compared to C++ standard views
-// - Iterating is stateles
-//   - Can iterate over elements when the view is const
-//   - Supports concurrent iterations
-//   - Read iterations do not affect later statements
 // - Always propagates const
 // Because
-// - This view does never cache begin()
 // - This view yields const iterators when it is const
 //*************************************************************
 namespace belleviews {
